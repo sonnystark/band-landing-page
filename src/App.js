a9356501118b22e357435cb81770007462af8267
sonnystark <sonny_stark@yahoo.com>
2021-11-30 11:11:37 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Header from './napalm_death/Header';
+import NavigationBar from './napalm_death/NavigationBar';
+import Band from './napalm_death/Band';
+import Vocals from './napalm_death/Vocals';
+import Guitar from './napalm_death/Guitar';
+import Bass from './napalm_death/Bass';
+import Drums from './napalm_death/Drums';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header />
+        <NavigationBar />
+        <Routes>
+            <Route path='/band' element={<Band />} />
+            <Route path='/vocals' element={<Vocals />} />
+            <Route path='/guitar' element={<Guitar />} />
+            <Route path='/bass' element={<Bass />} />
+            <Route path='/drums' element={<Drums />} />
+            <Route path='*' element={<Band />} />
+          </Routes>
     </div>
   );
 }
